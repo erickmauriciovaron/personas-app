72068583a2554b735333881a6271d723b99e8f8e
erickmauriciovaron <erick.mauricio30@hotmail.com>
2024-09-30 09:36:26 -0500

    Commune Delete

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,4 @@ Route::get('/', function () {
 Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas.index');
 Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
 Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
-
+Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
