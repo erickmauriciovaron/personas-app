f5df9efa514a84aaae7a0aeb4e3b72798dee2528
erickmauriciovaron <erick.mauricio30@hotmail.com>
2024-10-01 12:01:01 -0500

    Departamento List

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComunaController;
 use App\Http\Controllers\MunicipioController;
+use App\Http\Controllers\DepartamentoController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,3 +27,7 @@ Route::get('/municipios/create', [MunicipioController::class, 'create'])->name('
 Route::delete('/municipios/{municipio}', [MunicipioController::class, 'destroy'])->name('municipios.destroy');
 Route::put('/municipios/{municipio}', [MunicipioController::class, 'update'])->name('municipios.update');
 Route::get('/municipios/{municipio}/edit', [MunicipioController::class, 'edit'])->name('municipios.edit');
+
+//RUTAS DEPARTAMENTO
+
+Route::get('/departamentos', [DepartamentoController::class, 'index']);
